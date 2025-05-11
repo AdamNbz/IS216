cd21c4ba39509471a0c8659aa709733a9b189778
1811htsang <macsin233@gmail.com>
2025-05-11 20:23:42 +0700

    add project file to repo

---
+++ src/main/java/cacdanhdaden/javamainproject/JavaMainProject.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package cacdanhdaden.javamainproject;
+import userview.AuthenticationFrame;
+import supervisorview.*;
+/**
+ *
+ * @author shanghuang
+ */
+public class JavaMainProject {
+    public static void main(String[] args) {
+        AuthenticationFrame AF = new AuthenticationFrame();
+    }
+}
